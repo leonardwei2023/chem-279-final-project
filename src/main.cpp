a869352f82798d9dafdad3c12a3bbc8fbb1e4e89
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-29 12:22:26 -0500

    Add vibrational frequency analysis functionality

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,14 @@
 
+#include "vibrational_frequency.h"
+
+
+
+double step_size = 0.005;
+
+VibrationalFrequencyAnalyzer vib(step_size);
+
+arma::mat hessian = vib.compute_hessian(mol);
+arma::mat mass_weighted_hessian = vib.mass_weight_hessian(hessian, mol);
+arma::vec frequencies = vib.compute_frequencies(mass_weighted_hessian);
+
+vib.print_frequencies(frequencies);
