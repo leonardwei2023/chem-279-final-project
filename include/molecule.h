e9138367ab684ddb79824ff3bc3b1e846635ab44
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-29 13:32:25 -0500

    Update molecule.h

--- include/molecule.h
+++ include/molecule.h
@@ -1,1 +1,27 @@
 
+#ifndef MOLECULE_H
+#define MOLECULE_H
+
+#include <armadillo>
+#include <vector>
+#include <string>
+
+class Molecule {
+public:
+    arma::mat coordinates;
+    std::vector<std::string> symbols;
+    std::vector<double> masses;
+
+    Molecule();
+
+    void set_num_atoms(int n);
+    int num_atoms() const;
+
+    void set_symbol(int index, const std::string& symbol);
+    std::string atomic_symbol(int index) const;
+
+    void set_atomic_mass(int index, double mass);
+    double atomic_mass(int index) const;
+};
+
+#endif
