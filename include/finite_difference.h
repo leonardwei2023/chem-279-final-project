c7432e1d469a549c3c0d316c726dee6e5dc81144
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-29 12:17:35 -0500

    Update finite_difference.h

--- include/finite_difference.h
+++ include/finite_difference.h
@@ -1,14 +1,23 @@
-
 #ifndef FINITE_DIFFERENCE_H
 #define FINITE_DIFFERENCE_H
 
 #include <armadillo>
 #include "molecule.h"
 
-arma::mat compute_hessian_finite_difference(Molecule mol, double h);
-arma::mat mass_weight_hessian(const arma::mat& H, const Molecule& mol);
-arma::vec compute_frequencies_cm(const arma::mat& Hmw);
+// Builds the Cartesian Hessian using finite differences of the CNDO/2 energy:
 
-void print_frequencies(const arma::vec& freqs);
+arma::mat compute_hessian_finite_difference(Molecule mol, double step_size);
+
+// Converts the normal Hessian into a mass-weighted Hessian:
+
+arma::mat mass_weight_hessian(const arma::mat& hessian, const Molecule& mol);
+
+// Diagonalizes the mass-weighted Hessian and converts eigenvalues to cm^-1:
+
+arma::vec compute_frequencies_cm(const arma::mat& mass_weighted_hessian);
+
+// Prints all modes in a clean table:
+
+void print_frequencies(const arma::vec& frequencies);
 
 #endif
