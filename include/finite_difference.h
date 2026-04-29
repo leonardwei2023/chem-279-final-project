901e074af28c9ec5a82a605a7bf0c21dd1726021
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-29 12:00:48 -0500

    Update finite_difference.h

--- include/finite_difference.h
+++ include/finite_difference.h
@@ -1,1 +1,14 @@
 
+#ifndef FINITE_DIFFERENCE_H
+#define FINITE_DIFFERENCE_H
+
+#include <armadillo>
+#include "molecule.h"
+
+arma::mat compute_hessian_finite_difference(Molecule mol, double h);
+arma::mat mass_weight_hessian(const arma::mat& H, const Molecule& mol);
+arma::vec compute_frequencies_cm(const arma::mat& Hmw);
+
+void print_frequencies(const arma::vec& freqs);
+
+#endif
