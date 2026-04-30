31e7c23a4368ae390329409b5be95ff15ab4d0d9
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-29 21:18:06 -0500

    Update hessian.h

--- include/hessian.h
+++ include/hessian.h
@@ -1,1 +1,22 @@
 
+#ifndef HESSIAN_H
+#define HESSIAN_H
+
+#include <string>
+#include <vector>
+
+class Hessian {
+private:
+    int size;
+    std::vector<std::vector<double>> matrix;
+
+public:
+    Hessian();
+
+    void read_from_file(const std::string& filename, int expected_size);
+
+    int get_size() const;
+    std::vector<std::vector<double>> get_matrix() const;
+};
+
+#endif
