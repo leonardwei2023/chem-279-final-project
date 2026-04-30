aadfbd8f6a9cf3e4b901e55d7cb4e98a24c9d733
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-30 17:46:49 -0500

    Update vibration.h

--- include/vibration.h
+++ include/vibration.h
@@ -1,19 +1,25 @@
+#ifndef VIBRATION_H
+#define VIBRATION_H
 
-#ifndef VIBRATIONS_H
-#define VIBRATIONS_H
-
-#include "molecule.h"
 #include "hessian.h"
+#include "molecule.h"
 
+#include <string>
 #include <vector>
 
 class Vibrations {
 private:
     std::vector<double> frequencies;
 
+    bool is_linear_molecule(const Molecule& molecule) const;
+
 public:
     void compute(const Molecule& molecule, const Hessian& hessian);
+
     void print_frequencies() const;
+    void write_frequencies(const std::string& filename) const;
+
+    std::vector<double> get_frequencies() const;
 };
 
 #endif
