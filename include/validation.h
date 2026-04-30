773061fe51a04b1983ab6da3f6e49f3ea36fae83
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-30 17:16:44 -0500

    Update validation.h

--- include/validation.h
+++ include/validation.h
@@ -1,1 +1,15 @@
 
+#ifndef VALIDATION_H
+#define VALIDATION_H
+
+#include <vector>
+
+class Validation {
+public:
+    static void compare_with_reference(
+        const std::vector<double>& computed,
+        const std::vector<double>& reference
+    );
+};
+
+#endif
