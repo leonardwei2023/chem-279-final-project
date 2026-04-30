c67c0f0e6e5f878026e3a9ba5a174dc443e8894c
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-29 21:18:45 -0500

    Update vibration.h

--- include/vibration.h
+++ include/vibration.h
@@ -1,1 +1,19 @@
 
+#ifndef VIBRATIONS_H
+#define VIBRATIONS_H
+
+#include "molecule.h"
+#include "hessian.h"
+
+#include <vector>
+
+class Vibrations {
+private:
+    std::vector<double> frequencies;
+
+public:
+    void compute(const Molecule& molecule, const Hessian& hessian);
+    void print_frequencies() const;
+};
+
+#endif
