db740423e9ce353fd73cc256bf5655761ecbdae9
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-30 14:38:37 -0500

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -2,21 +2,62 @@
 #include "hessian.h"
 #include "vibration.h"
 
+// later: #include "dipole.h"
+
 #include <iostream>
+#include <string>
 
-int main() {
+int main(int argc, char* argv[]) {
     try {
-        Molecule molecule;
-        molecule.read_xyz("input/h2o.xyz");
+        if (argc < 3) {
+            std::cout << "Usage:\n";
+            std::cout << "./program vibration xyz hessian\n";
+            std::cout << "./program dipole xyz\n";
+            return 1;
+        }
 
-        int hessian_size = 3 * molecule.get_num_atoms();
+        std::string mode = argv[1];
 
-        Hessian hessian;
-        hessian.read_from_file("input/h2o_hessian.dat", hessian_size);
+        if (mode == "vibration") {
+            if (argc != 4) {
+                std::cout << "Usage: ./program vibration xyz hessian\n";
+                return 1;
+            }
 
-        Vibrations vibrations;
-        vibrations.compute(molecule, hessian);
-        vibrations.print_frequencies();
+            std::string xyz_file = argv[2];
+            std::string hessian_file = argv[3];
+
+            Molecule molecule;
+            molecule.read_xyz(xyz_file);
+
+            int size = 3 * molecule.get_num_atoms();
+
+            Hessian hessian;
+            hessian.read_from_file(hessian_file, size);
+
+            Vibrations vibrations;
+            vibrations.compute(molecule, hessian);
+            vibrations.print_frequencies();
+        }
+        else if (mode == "dipole") {
+            std::string xyz_file = argv[2];
+
+            Molecule molecule;
+            molecule.read_xyz(xyz_file);
+
+            // placeholder for now
+
+            
+            std::cout << "Dipole calculation not implemented yet.\n";
+
+            // later: Add these for dipole moment:
+            // Dipole dipole;
+            // dipole.compute(molecule);
+            // dipole.print();
+        }
+        else {
+            std::cout << "Unknown mode: " << mode << std::endl;
+        }
     }
     catch (const std::exception& error) {
         std::cout << "Error: " << error.what() << std::endl;
