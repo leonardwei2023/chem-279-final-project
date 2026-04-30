96e39f15f0ed23c42897ed44bf42b9c5055e5eca
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-30 17:46:15 -0500

    Update finite_difference.h

--- include/finite_difference.h
+++ include/finite_difference.h
@@ -1,1 +1,19 @@
 
+#ifndef FINITE_DIFFERENCE_H
+#define FINITE_DIFFERENCE_H
+
+#include "cndo_engine.h"
+#include "hessian.h"
+#include "molecule.h"
+
+class FiniteDifference {
+private:
+    double step_size;
+
+public:
+    FiniteDifference(double step);
+
+    Hessian compute_hessian(const Molecule& molecule, CNDOEngine& engine);
+};
+
+#endif
