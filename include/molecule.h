1bd6043e8d46036a9145920724b89b21bb9e5c24
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-30 17:44:21 -0500

    Update molecule.h

--- include/molecule.h
+++ include/molecule.h
@@ -8,9 +8,8 @@ class Molecule {
 private:
     int num_atoms;
     std::vector<std::string> symbols;
-    // x1 y1 z1 x2 y2 z2 ...
-    std::vector<double> coordinates; 
-    std::vector<double> masses;
+    std::vector<double> coordinates; // Angstrom: x1 y1 z1 x2 y2 z2 ...
+    std::vector<double> masses;      // amu
 
     double get_mass_from_symbol(const std::string& symbol) const;
 
@@ -18,11 +17,17 @@ public:
     Molecule();
 
     void read_xyz(const std::string& filename);
+    void write_xyz(const std::string& filename) const;
 
     int get_num_atoms() const;
+    int get_num_coordinates() const;
+
     std::vector<std::string> get_symbols() const;
     std::vector<double> get_coordinates() const;
     std::vector<double> get_masses() const;
+
+    double get_coordinate(int index) const;
+    void set_coordinate(int index, double value);
 };
 
 #endif
