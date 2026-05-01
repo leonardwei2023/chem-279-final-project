3e1aa51b7d2810da11e040338ce3145f5fcc3231
DAVID HOUSHANGI <davidhoushangi@berkeley.edu>
2026-04-30 21:49:01 -0500

    Update finite_difference.cpp

--- src/finite_difference.cpp
+++ src/finite_difference.cpp
@@ -5,19 +5,21 @@ FiniteDifference::FiniteDifference(double step) {
     step_size = step;
 }
 
-Hessian FiniteDifference::compute_hessian(const Molecule& molecule, CNDOEngine& engine) {
+Hessian FiniteDifference::compute_hessian(
+    const Molecule& molecule,
+    CNDOEngine& engine
+) {
     int size = molecule.get_num_coordinates();
 
     Hessian hessian;
     hessian.resize(size);
 
     const double ang_to_bohr = 1.889726124565062;
-    double step_bohr = step_size * ang_to_bohr;
+    const double ang_to_bohr_sq = ang_to_bohr * ang_to_bohr;
 
     std::cout << "Computing finite-difference Hessian\n";
     std::cout << "Coordinates: " << size << "\n";
     std::cout << "Step size: " << step_size << " Angstrom\n";
-    std::cout << "Step size: " << step_bohr << " Bohr\n";
 
     double e0 = engine.compute_energy(molecule);
 
@@ -29,14 +31,23 @@ Hessian FiniteDifference::compute_hessian(const Molecule& molecule, CNDOEngine& 
                 Molecule mol_plus = molecule;
                 Molecule mol_minus = molecule;
 
-                mol_plus.set_coordinate(i, mol_plus.get_coordinate(i) + step_size);
-                mol_minus.set_coordinate(i, mol_minus.get_coordinate(i) - step_size);
+                mol_plus.set_coordinate(
+                    i,
+                    mol_plus.get_coordinate(i) + step_size
+                );
+
+                mol_minus.set_coordinate(
+                    i,
+                    mol_minus.get_coordinate(i) - step_size
+                );
 
                 double e_plus = engine.compute_energy(mol_plus);
                 double e_minus = engine.compute_energy(mol_minus);
 
-                value = (e_plus - 2.0 * e0 + e_minus) /
-                        (step_bohr * step_bohr);
+                double raw = (e_plus - 2.0 * e0 + e_minus) /
+                             (step_size * step_size);
+
+                value = raw / ang_to_bohr_sq;
             } else {
                 Molecule mol_pp = molecule;
                 Molecule mol_pm = molecule;
@@ -60,16 +71,18 @@ Hessian FiniteDifference::compute_hessian(const Molecule& molecule, CNDOEngine& 
                 double e_mp = engine.compute_energy(mol_mp);
                 double e_mm = engine.compute_energy(mol_mm);
 
-                value = (e_pp - e_pm - e_mp + e_mm) /
-                        (4.0 * step_bohr * step_bohr);
+                double raw = (e_pp - e_pm - e_mp + e_mm) /
+                             (4.0 * step_size * step_size);
+
+                value = raw / ang_to_bohr_sq;
             }
 
             hessian.set_value(i, j, value);
             hessian.set_value(j, i, value);
         }
 
-        std::cout << "Finished Hessian row " << i + 1
-                  << " of " << size << "\n";
+        std::cout << "Finished Hessian row "
+                  << i + 1 << " of " << size << "\n";
     }
 
     return hessian;
